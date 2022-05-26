834f0791e44b033d9c9ebf33187ed54aefc2ae11
ArSiFoX <arslangalimov0@gmail.com>
2022-05-26 12:30:11 +0300

    Update theme-ROBLOXSENSE.js

--- ace/theme-ROBLOXSENSE.js
+++ ace/theme-ROBLOXSENSE.js
@@ -3,7 +3,7 @@ ace.define("ace/theme/ROBLOXSENSE",["require","exports","module","ace/lib/dom"],
 exports.isDark = true;
 exports.cssClass = "ace-twilight";
 exports.cssText = ".ace-twilight .ace_gutter {\
-background: #1E1E1E;\
+background: #363e45;\
 color: #E2E2E2\
 }\
 .ace-twilight .ace_print-margin {\
@@ -11,7 +11,7 @@ width: 1px;\
 background: #1E1E1E\
 }\
 .ace-twilight {\
-background-color: #1E1E1E;\
+background-color: #434e54;\
 color: #E2E2E2\
 }\
 .ace-twilight .ace_cursor {\
